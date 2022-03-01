33701434d99cd34d2122a81c2be0f997f2bcb6ba
afeng <afeng_hyf@163.com>
2022-03-01 20:27:24 +0800

    first commit to github

---
+++ src/main/java/cn/yizhouiqpl/Sp_admin_sb.java
@@ -0,0 +1,13 @@
+package cn.yizhouiqpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp_admin_sb {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp_admin_sb.class, args);
+    }
+
+}
